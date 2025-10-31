76eadf5d60a3bbcce66cf6d7645d1f43d461c686
sophiasosafiscella <sophia.sosa@nanograv.org>
2025-10-31 11:34:58 -0400

    Latest version

--- main.py
+++ main.py
@@ -1,132 +1,110 @@
 import numpy as np
-import numpy.polynomial.legendre as leg
-import pandas as pd
-import seaborn as sns
+from numpy.polynomial.legendre import legval
 import matplotlib.pyplot as plt
-from matplotlib.offsetbox import AnchoredText
-from lmfit import Model, Parameters
-from numpy.polynomial.legendre import Legendre
-from numpy.polynomial.polynomial import Polynomial
-from pint.models import get_model
-from pint.residuals import Residuals
-from pint.toa import get_TOAs
-import pint
-pint.logging.setup(level="ERROR")
-from utils import epoch_scrunch, get_data,  make_plot
-from fit_coefficients import my_legfit_full
-import astropy.units as u
 from glob import glob
-import os
-import pickle
-import sys
+from pint.fitter import WLSFitter
+from pint.models import get_model
+from pint.toa import get_TOAs
+from new_LIFD import LIFD
 
 
-def find_dispersion_coefficients(psr_name:str, fixed_coeffs:bool=True, plot:bool=False):
+# Global parameters
+#PSR_name: str = "J0030+0451"
+#PSR_name: str = "B1937+21"
+#PSR_name: str = "J1643-1224"
+#PSR_name: str = "J1024-0719"
+#PSR_name: str = "J1903+0327"
+#PSR_name: str = "J1741+1351"
+#PSR_name: str = "J1744-1134"
+#PSR_name: str = "J0613-0200"
+#PSR_name: str = "J2145-0750"
+#PSR_name: str = "J1909-3744"
+PSR_name: str = "J1918-0642"
 
-    # Input files
-    parfile: str = glob(f"./NANOGrav15yr_PulsarTiming_v2.0.1/narrowband/par/{psr_name}_PINT_*.nb.par")[0]
-    timfile: str = glob(f"./NANOGrav15yr_PulsarTiming_v2.0.1/narrowband/tim/{psr_name}_PINT_*.nb.tim")[0]
-    pickle_file: str = f"./results/{PSR_name}/{PSR_name}_data_obj.pkl"
+print(f"Running {PSR_name}...")
 
-    # Load the timing model and TOAs
-    timing_model = get_model(parfile)  # Ecliptical coordiantes
-    toas = get_TOAs(timfile, planets=True, ephem=timing_model.EPHEM.value)
+output_par: str = f"./results/{PSR_name}/new_LIFD.par"
+n_LIFD: int = 5
 
-    if plot:
-        original_residuals = Residuals(toas, timing_model).time_resids.to(u.us).value
-        epochs, retval, retvalerrs = epoch_scrunch(toas.get_mjds().value, data=original_residuals, errors=toas.get_errors().to(u.us).value, weighted=True)
+# Input files
+parfile: str = glob(f"./NANOGrav15yr_PulsarTiming_v2.0.1/narrowband/par/{PSR_name}_PINT_*.nb.par")[0]
+timfile: str = glob(f"./NANOGrav15yr_PulsarTiming_v2.0.1/narrowband/tim/{PSR_name}_PINT_*.nb.tim")[0]
+# Load the timing model and TOAs
+timing_model = get_model(parfile)  # Ecliptical coordiantes
+toas = get_TOAs(timfile, planets=True, ephem=timing_model.EPHEM.value)
 
-        sns.set_style('darkgrid')
-        plt.errorbar(epochs, retval, yerr=retvalerrs, fmt='x')
-        plt.xlabel('MJD')
-        plt.ylabel('Residuals [$\mu \mathrm{s}$]')
-        plt.title('Original Residuals vs. MJD')
-        plt.show()
+# Change the DM
+print(timing_model.DM)
 
-    # Get the TOAs obtained with GUPPI and that are in DMX windows with observations in both frequency bands
-    if os.path.exists(pickle_file):
-        with open(pickle_file, "rb") as f:
-            filtered_obs = pickle.load(f)
-    else:
-        filtered_obs = get_data(psr_name, toas, timing_model)
-        with open(pickle_file, "wb") as f:
-            pickle.dump(filtered_obs, f)
+change_dm: bool = True
+if change_dm:
+    if PSR_name == "J2145-0750":
+        params = {"DM": (8.999792308264002 * timing_model.DM.units, 1, 0 * timing_model.DM.units)}
+    elif PSR_name == "J1909-3744":
+        params = {"DM": (10.391378695991417 * timing_model.DM.units, 1, 0 * timing_model.DM.units)}
+    elif PSR_name == "J1918-0642":
+        #params = {"DM": (26.5889 * timing_model.DM.units, 1, 0 * timing_model.DM.units)}
+        #params = {"DM": (26.589879853155644 * timing_model.DM.units, 1, 0 * timing_model.DM.units)}
+        params = {"DM": (26.9 * timing_model.DM.units, 1, 0 * timing_model.DM.units)}
+    elif PSR_name == "J1744-1134":
+        params = {"DM": (3.139 * timing_model.DM.units, 1, 0 * timing_model.DM.units)}
+        #params = {"DM": (3.14021942492064 * timing_
+        # model.DM.units, 1, 0 * timing_model.DM.units)}
 
-    # Arrays to store the coefficients
-    coeffs_arr = np.empty((len(filtered_obs.dmx_ranges), 6))
+    for name, info in params.items():
+        par = getattr(timing_model, name)  # Get parameter object from name
+        par.value = info[0]  # set parameter value
+        if len(info) > 1:
+            if info[1] == 1:
+                par.frozen = True  # Frozen means do not fit
+            par.uncertainty = info[2]  # set parameter uncertainty
 
-    # Iterate over the DMX windows
-    for n, (x, y) in enumerate(zip(filtered_obs.xvals, filtered_obs.resids)):
+    print(timing_model.DM)
 
-        window = filtered_obs.dmx_ranges[n]
-        print("Working on " + str(window[0]) + " to " + str(window[1]) + " (index " + str(n) + ")...")
+# Remove the FD model
+timing_model.remove_component("FD")
 
-         # Calculate the coefficients for the unscaled and unshifted Legendre basis polynomials
-        pfit = leg.Legendre.fit(x=x, y=y.astype(np.float64), deg=5, full=False)
-        leg_pfit_coef = pfit.coef
+# The LIFD component expects to define its λ→x mapping at setup(), which requires TOAs.
+# Therefore, we will attach TOAs to model before adding LIFD:
+timing_model.toas = toas
 
-        #  Coefficients of the equivalent polynomial (relative to the “standard” basis) ordered from lowest to highest degree.
-        coeffs_arr[n, :] = leg.leg2poly(leg_pfit_coef)
+# Attach the LIFD component
+lifd = LIFD(order=n_LIFD)
+timing_model.add_component(lifd)
 
-        if plot:
-            new_x, new_y = zip(*sorted(zip(x, y)))
-            new_x, new_y = np.array(new_x), np.array(new_y)
+# Now LIFD1, LIFD2, … LIFD4 are free parameters
+#print(timing_model.params)
+#print(timing_model.free_params)
 
-            at = AnchoredText(f"Power series coefficients: \n $C_0$ = {coeffs_arr[n, 0]} \n $C_1$ = {coeffs_arr[n, 1]} \n $C_2$ = {coeffs_arr[n, 2]} \n $C_3$ = {coeffs_arr[n, 3]} \n $C_4$ = {coeffs_arr[n, 4]} \n $C_5$ = {coeffs_arr[n, 5]}",
-                              prop=dict(size=10), frameon=True, loc='upper left')
+# Fitting
+f = WLSFitter(toas, timing_model)
+f.fit_toas()
+#    f.model.write_parfile(output_par, "wt")
+new_model = f.model
+print(new_model.free_params)
 
-#            at = AnchoredText(f"Legendre series coefficients: \n $C_0$ = {leg_pfit_coef[0]} \n $C_1$ = {leg_pfit_coef[1]} \n $C_2$ = {leg_pfit_coef[2]} \n $C_3$ = {leg_pfit_coef[3]} \n $C_4$ = {leg_pfit_coef[4]} \n $C_5$ = {leg_pfit_coef[5]}",#                              prop=dict(size=10), frameon=True, loc='upper left')
-#            at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+# Get the x values
+freq_hz = LIFD.get_freq_hz_from_toas(new_model, toas)  # Frequencies in Hz
+lambdas = LIFD.get_lambda_from_freq_hz(freq_hz)  # Inverse frequencies
+lmin = lambdas.min()  # lambda_min
+lmax = lambdas.max()  # lambda_max
+x_vals = np.sort(LIFD.map_lambda_to_unit(lambdas, lmin, lmax))  # fixed mapping set in setup()
 
-            # Make some cool plots
-            '''
-            fig, ax = plt.subplots()
-            ax.plot(frequencies, residuals, "o")
-            ax.set_title("MJD " + str(window[0]) + " to " + str(window[1]))
-            ax.set_xlabel("Frequency [GHz]")
-            ax.set_ylabel(r'Residuals [$\mu s$]')
-            ax.axvspan(0.725, 0.916, alpha=0.4, color='C2', label="GUPPI - Revr_800")
-            ax.axvspan(1.156, 1.882, alpha=0.4, color='C3', label="GUPPI - Revr1_2")
-            ax.legend(loc="upper right")
-            '''
+# Get the y values
+coeffs = [getattr(new_model, f"LIFD{i}").value for i in range(1, n_LIFD+1)]
+y_vals = legval(x_vals, coeffs)
+y_vals_musec = y_vals * 1e6
 
-            fig2, ax2 = plt.subplots()
-            ax2.plot(new_x, new_y, "o")
-            ax2.plot(new_x, pfit(new_x), lw=2, label="Fitted Legendre Polynomial")
-#            ax2.plot(x_values, power_series_pol(x_values), "-", lw=2, label="Power Series Polynomial fit")
-            ax2.set_title("MJD " + str(window[0]) + " to " + str(window[1]))
-            ax2.set_xlabel("Legendre-Mapped Inverse Frequency")
-            ax2.set_ylabel(r'Residuals [$\mu s$]')
-            ax2.add_artist(at)
-            ax2.legend(loc="upper right")
-            plt.tight_layout()
-#            plt.savefig('./figures/NG15/fit_' + str(n) + '.png')
-            plt.show()
+fig, ax = plt.subplots(1,1)
+ax.plot(x_vals, y_vals_musec)
+ax.set_xlabel("x")
+ax.set_ylabel("Delay [$\mu$s]")
+plt.suptitle(PSR_name + f" | DM={timing_model.DM.value}")
+plt.tight_layout()
+plt.show()
 
-    # Create a DataFrame to hold all the information and drop the windows where any of the coefficients is a Nan,
-    # because that means that that particular window was skipped due to not having observations in both bands
-    df = pd.DataFrame(np.hstack((filtered_obs.dmx_ranges, coeffs_arr)), columns=["DMXR1", "DMXR2", "a0", "a1", "a2", "a3", "a4", "a5"])
-
-    return df.dropna(axis='index', how='any')
-
-
-if __name__ == "__main__":
-
-    PSR_name: str = "B1937+21"
-#    PSR_name: str = "J1643-1224"
-#    PSR_name: str = "J1024-0719"
-#    PSR_name: str = "J1903+0327"
-#    PSR_name: str = "J1741+1351"
-#    PSR_name: str = "J1744-1134"
-#    PSR_name: str = "J0613-0200"
-#    PSR_name: str = "J2145-0750"
-    if not os.path.exists('./results/' + PSR_name + '_results.csv'):
-        df = find_dispersion_coefficients(PSR_name)
-        df.to_csv('./results/' + PSR_name + '_results.csv')
-    else:
-        df = pd.read_csv('./results/' + PSR_name + '_results.csv')
-
-    make_plot(PSR_name, df)
-
-
-
+print(new_model.LIFD1)
+print(new_model.LIFD2)
+print(new_model.LIFD3)
+print(new_model.LIFD4)
+print(new_model.LIFD5)
